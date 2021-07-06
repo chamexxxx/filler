5fb9a52214d64cdebca6ba2821de3326edfcaab7
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-07-06 13:48:43 +0400

    Add UsesUUID trait for models

---
+++ app/Models/Concerns/UsesUUID.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Concerns;
+
+use Illuminate\Support\Str;
+
+trait UsesUUID
+{
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return "string";
+    }
+}
