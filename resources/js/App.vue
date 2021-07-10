459c0c104a56fb23e28d124b65adcace00428ec1
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-07-10 11:00:13 +0400

    Make SPA

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,12 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue').default;
+window.Vue = require("vue").default;
+
+import router from "./router";
+import App from "./App.vue";
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,8 +22,6 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -28,5 +29,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    router,
+    el: "#app",
+    render: h => h(App)
 });
