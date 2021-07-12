f453d92ba7b95ebd7543fd7d07ac2dafe91e35f6
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-07-12 14:12:00 +0400

    Add game launch

--- resources/js/pages/Start.vue
+++ resources/js/pages/Start.vue
@@ -6,8 +6,21 @@
                     «Филлер» — игра для двух игроков
                 </h4>
                 <description />
-                <button @click="onStarted" class="btn btn-primary btn-lg mt-4 mb-3">
-                    Начать игру
+                <button
+                    @click="onStarted"
+                    class="btn btn-primary btn-lg mt-4 mb-3"
+                    type="button"
+                    :disabled="isLoading"
+                >
+                    <div v-if="isLoading" class="flex items-center">
+                        <span
+                            class="spinner-grow spinner-grow-sm mr-2"
+                            role="status"
+                            aria-hidden="true"
+                        ></span>
+                        Идет создание игры...
+                    </div>
+                    <template v-else>Начать игру</template>
                 </button>
             </div>
         </div>
@@ -20,9 +33,24 @@ import Description from "../components/Description.vue";
 export default {
     name: "Start",
     components: { Description },
+    data() {
+        return {
+            isLoading: false
+        };
+    },
     methods: {
         onStarted() {
-            this.$router.push({ name: "Game" });
+            this.createGame(15, 15).then(response => {
+                const { id } = response.data;
+                this.$router.push({ name: "Game", params: { gameId: id } });
+            });
+        },
+        createGame(width, height) {
+            this.isLoading = true;
+
+            return axios
+                .post("api/game", { width, height })
+                .finally(() => (this.isLoading = false));
         }
     }
 };
